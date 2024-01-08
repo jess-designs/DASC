0d466f75b8f12327b2a8f281ffda60fc3f90a11a
Jess <jessica.annd760@gmail.com>
2024-01-08 22:03:44 +0100

    Removed unnecessary comments

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.set('view engine', 'ejs');
-// app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static("public"));
 
